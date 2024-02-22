a13a2a8a56651311af02892583ce3b8deca9dd55
shrouk Ashraf <78410468+shroukashraf912@users.noreply.github.com>
2024-02-22 23:59:22 +0200

    Setup function

---
+++ esp_tabs_test/y_setup.ino
@@ -0,0 +1,27 @@
+void setup()
+{
+  // start serial monitor
+  Serial.begin(BAUD_RATE);
+  // while (!Serial);
+   pinMode(BUZZER_PIN, OUTPUT);
+  // Initialize the TFT screen
+  TFT_Init();
+  // scan wifi
+  wifi_scan();
+  
+  //connect to wifi
+  wifi_connect();
+
+  //set the time
+  time_setup();
+
+  //clock alarm 
+  alarm_setup();
+
+  // // setup sensors
+  max30102_check();
+  mlx90614_check();
+
+  Serial.println("Setup done");
+  IdKeypad_Init();
+}
