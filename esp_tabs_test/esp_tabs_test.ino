c42355b9a02447b4eef6248eb868a2a1863168b9
shrouk Ashraf <78410468+shroukashraf912@users.noreply.github.com>
2024-02-22 23:49:22 +0200

    Library

---
+++ esp_tabs_test/esp_tabs_test.ino
@@ -0,0 +1,29 @@
+/* 
+ ********************************************************************************** 
+ *  AUTHER      : Shrouk Ashraf & Ahmed Mohamed 
+ *  DATE        : 22/1/2024
+ *  DESCRIPTION : Medical watch
+ *  VERSION     : 1.3 esp //ID
+ **********************************************************************************
+ */
+
+/********************************** INCLUDES **************************************/
+/* WIFI Library */
+#include <WiFi.h>
+
+/* TFT SPI libraries */
+#include "FS.h"
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+/* MLX90614 Library */
+#include <Adafruit_MLX90614.h>
+
+/* MAX30102 Library */
+#include <DFRobot_BloodOxygen_S.h>
+
+/* NTP time library */
+#include "time.h"
+#include "sntp.h"
+
+/**********************************************************************************/
